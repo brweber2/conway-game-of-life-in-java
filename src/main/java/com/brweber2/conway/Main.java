3039ffccde8fcfe54aac5bbce6b55ea76e4ba377
Bryan Weber <bweber@arin.net>
2012-03-26 16:47:56 -0400

    refactoring code, plus trying to figure out odd behavior of java's Random class when in a loop

--- src/main/java/com/brweber2/conway/Main.java
+++ src/main/java/com/brweber2/conway/Main.java
@@ -10,12 +10,12 @@ import java.util.Set;
 
 public class Main
 {
-    
+
     public static int WIDTH = 35;
     public static int HEIGHT = 20;
     public static int COORDS = 345;
     public static Random random = new Random();
-    
+
     public static void main( String[] args ) throws InterruptedException
     {
         Game game = new Game( randomIntialState() );
@@ -24,16 +24,17 @@ public class Main
             game.printBoard();
             game.advanceToNextRound();
             Thread.sleep( 500 );
-        } 
+        }
         while ( ! game.over() );
     }
 
     private static Collection<Coordinate> randomIntialState()
     {
         Set<Coordinate> list = new HashSet<Coordinate>();
-        for ( int i = 0; i < random.nextInt(COORDS); i++ )
+        int n = random.nextInt( COORDS ); // inlining this on the next line causes a loss in randomness!?!?
+        for ( int i = 0; i < n; i++ )
         {
-            list.add( new Coordinate( random.nextInt(WIDTH), random.nextInt(HEIGHT) ) );
+            list.add( new Coordinate( random.nextInt( WIDTH ), random.nextInt( HEIGHT ) ) );
         }
         return list;
     }
